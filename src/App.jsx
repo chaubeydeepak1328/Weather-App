306bf8d0bd77679b9a80a284676c4076e506b5d1
Deepak <chaubeydeepak903@gmail.com>
2024-03-16 11:04:26 +0530

    updating changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Navbar from "./Navbar";
-
+import Loader from "./Loader";
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(() => {
+    setIsLoading(false);
+  }, 2000);
   return (
     <>
-      <Navbar />
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh", // Adjust this according to your layout
+          }}
+        >
+          <Loader />
+        </div>
+      ) : <Navbar />
+      }
+
     </>
   );
 }
